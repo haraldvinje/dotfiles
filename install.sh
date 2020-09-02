97a3e9adbc565f4becec0b0e04effe5f5ab6f4fc
Harald Vinje <haraldvinje@gmail.com>
2020-09-02 11:57:22 +0200

    update install script

--- install.sh
+++ install.sh
@@ -16,8 +16,8 @@ else
 fi
 
 #Installing vim-plug for neovim
-package=neovim
-if pacman -Qs $package > /dev/null ; then
+nvim=neovim
+if pacman -Qs $nvim > /dev/null ; then
     sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 else
